a410368f66025de8cde92b03c638c37d64164ac4
zhouy546 <zhouyunqi546@gmail.com>
2018-11-26 20:18:03 +0800

    something interesting

---
+++ Assets/Script/ChangeTheValue.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTheValue : MonoBehaviour {
+    public Projector projector;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            projector.fieldOfView += 0.1f;
+
+        } else if (Input.GetKey(KeyCode.DownArrow)) {
+            projector.fieldOfView -= 0.1f;
+
+        } else if (Input.GetKey(KeyCode.E)){
+
+            projector.transform.position = new Vector3(projector.transform.position.x, projector.transform.position.y, projector.transform.position.z + 0.5f);
+
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            projector.transform.position = new Vector3(projector.transform.position.x, projector.transform.position.y, projector.transform.position.z - 0.5f);
+        }
+
+
+    }
+}
---
+++ Assets/Script/DeBugText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeBugText : MonoBehaviour {
+
+    public static DeBugText instance;
+    public Projector projector;
+    public Text DebugText;
+	// Use this for initialization
+	void Start () {
+        if (instance == null) {
+            instance = this;
+        }
+        
+
+    }
+
+    private void Update()
+    {
+        UpdateDebugText();
+    }
+
+
+
+    public void UpdateDebugText()
+    {
+        string str = "field of view: " + projector.fieldOfView.ToString() + "\n" + "Camera z Distance: " + projector.transform.position.z.ToString();
+
+        DebugText.text = str;
+    }
+}
